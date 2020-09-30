fb15416502e452bf95d556da735ee4c6067241e9
Tony G <63308979+tsgrewing@users.noreply.github.com>
2020-09-29 22:54:00 -0500

    updating app.js

---
+++ assets/scripts/script.js
@@ -0,0 +1,26 @@
+// switch to demo gifs while mouse is over an image on the portfolio page
+// $(".portfolio-img").on({
+//     mouseover: function() {
+//       var state = $(this).attr('data-state');
+//       if (state === 'still') {
+//         $(this).attr('src', $(this).attr('data-demo'));
+//         $(this).attr('data-state', 'demo');
+//       }
+//       else {
+//         $(this).attr('src', $(this).attr('data-still'));
+//         $(this).attr('data-state', 'still');
+//       }
+//     },
+
+    // mouseout: function() {
+    //   var state = $(this).attr('data-state');
+    //   if (state === 'still') {
+    //     $(this).attr('src', $(this).attr('data-demo'));
+    //     $(this).attr('data-state', 'demo');
+    //   }
+    //   else {
+    //     $(this).attr('src', $(this).attr('data-still'));
+    //     $(this).attr('data-state', 'still');
+    //   }
+    // }
+// });
