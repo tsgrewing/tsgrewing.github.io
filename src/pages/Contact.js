9362fa2d39b507e041c5f1c2b8fabc7ac567f987
Tony G <63308979+tsgrewing@users.noreply.github.com>
2021-01-05 22:49:42 -0600

    updating contact page to add email address

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,6 +6,7 @@ function Contact() {
     return (
         <section class="row justify-content-center">
             <Header title="Contact Me" />
+            <h4>You can also reach me directly at: tsgrewing@gmail.com</h4>
             <ContactForm />  
         </section>
     );
