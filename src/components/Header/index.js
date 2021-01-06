32d76cd09756916b527c9b58295f0be56a4e97c5
Tony G <63308979+tsgrewing@users.noreply.github.com>
2021-01-05 23:24:08 -0600

    updating email

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-export default function Header({ title }) {
+export default function Header({ title, subtitle }) {
     return (
         <header class="col-xs-12 col-sm-11 col-md-11 col-lg-11">
         <h2 class="mx-left">{title}</h2>
+        <h5 class="col-xs-12 col-sm-11 col-md-11 col-lg-11">{subtitle}</h5>
         </header>
     )
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,8 +5,11 @@ import ContactForm from "../components/ContactForm";
 function Contact() {
     return (
         <section class="row justify-content-center">
-            <Header title="Contact Me" />
-            <h5 class="col-xs-12 col-sm-11 col-md-11 col-lg-11">tsgrewing@gmail.com</h5>
+            <Header 
+                title="Contact Me"
+                subtitle="tsgrewing@gmail.com"
+            />
+            
             <ContactForm />  
         </section>
     );
