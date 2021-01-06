0c73467d0ed2d1a9814691a874bb1e9f9dc4891e
Tony G <63308979+tsgrewing@users.noreply.github.com>
2021-01-05 23:55:25 -0600

    update email

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ export default function Header({ title, subtitle }) {
     return (
         <header class="col-xs-12 col-sm-11 col-md-11 col-lg-11">
         <h2 class="mx-left">{title}</h2>
-        <a href="mailto:tsgrewing@gmail.com"  target="_blank"><h5 class="col-xs-12 col-sm-11 col-md-11 col-lg-11">{subtitle}</h5></a>
+        <a href="mailto:tsgrewing@gmail.com"  target="_blank"><h5 class="mx-left">{subtitle}</h5></a>
         </header>
     )
 }
