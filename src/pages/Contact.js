8efaad1ee34b423273455de48139d3e62518192a
Tony G <63308979+tsgrewing@users.noreply.github.com>
2021-01-05 23:00:44 -0600

    updating email

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,7 +6,7 @@ function Contact() {
     return (
         <section class="row justify-content-center">
             <Header title="Contact Me" />
-            <h4>You can also reach me directly at: tsgrewing@gmail.com</h4>
+            <h5 class="col-xs-12 col-sm-11 col-md-11 col-lg-11">tsgrewing@gmail.com</h5>
             <ContactForm />  
         </section>
     );
